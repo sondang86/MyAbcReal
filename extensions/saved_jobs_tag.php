b507e1e7b23775b9b39ed50af1f6dede382c3df7
Dang Viet Son <dang.viet.son.hp@gmail.com>
2016-04-12 00:24:36 +0700

    Working on Get user saved jobs in the last 7 days

--- extensions/saved_jobs_tag.php
+++ extensions/saved_jobs_tag.php
@@ -6,21 +6,22 @@
 // http://www.netartmedia.net
 ?><?php
 if(!defined('IN_SCRIPT')) die("");
+global $db, $commonQueries;
 
-$link_suffix="";
+//http://dba.stackexchange.com/questions/97211/get-rows-where-lastlogintimestamp-is-in-last-7-days
 
+//Get user saved jobs in the last 7 days
+ $db->where("date >= CAST(UNIX_TIMESTAMP(NOW() - INTERVAL 7 DAY) AS CHAR(10))");
+//    ->where("user_uniqueId",filter_input(INPUT_COOKIE,'userId', FILTER_SANITIZE_STRING))
+//    ->where("IPAddress", filter_input(INPUT_SERVER,'REMOTE_ADDR', FILTER_VALIDATE_IP));
 
-if($MULTI_LANGUAGE_SITE)
-{
-	$link_suffix="lang=".$this->lang;
-}
+$testdata = $db->get("jobs");
+print_r($testdata);
+
 
 if(isset($_COOKIE["saved_listings"])&&trim($_COOKIE["saved_listings"])!="")
 {
-
-	$items=explode(",",$_COOKIE["saved_listings"]);
+    $items=explode(",",$_COOKIE["saved_listings"]);
 ?>
-	<a href="<?php echo $website->check_SEO_link("saved_jobs", $SEO_setting);?>" class="sub-text underline-link r-margin-15"><?php echo (sizeof($items)-1)." ".$M_SAVED_JOBS;?></a>
-<?php
-}
-?>
+    <a href="<?php echo $website->check_SEO_link("saved_jobs", $SEO_setting);?>" class="sub-text underline-link r-margin-15"><?php echo (sizeof($items)-1)." ".$M_SAVED_JOBS;?></a>
+<?php }?>
