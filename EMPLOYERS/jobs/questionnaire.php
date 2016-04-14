f4d23038393e2b7a6a182dc66ea85732350a509f
Dang Viet Son <dang.viet.son.hp@gmail.com>
2016-04-15 00:06:43 +0700

    working on employer questionnaires

--- EMPLOYERS/jobs/questionnaire.php
+++ EMPLOYERS/jobs/questionnaire.php
@@ -13,88 +13,24 @@ if(!defined('IN_SCRIPT')) die("");
 
 <h3 class="no-top-margin"><?php echo $M_ADD_NEW_QUESTION;?></h3>
 <br/>
-<?php
-$job_id=$_REQUEST["id"];
+<div class="row">
+    <div class="col-md-1">Câu hỏi: </div>
+    <div class="col-md-6"><input type="text" style="width: 100%;"></div>
+    <div class="col-md-3">
+        <select>
+            <option>Text area</option>
+            <option>poll</option>
+        </select>
+    </div>
+</div>
 
+<div class="row answerTextArea" style="margin-top: 20px;">
+    <div class="col-md-1">Câu trả lời: </div>
+    <div class="col-md-6"><textarea style="width: 100%; min-height: 400px;"></textarea></div>
+</div>
 
-if(isset($_POST["Delete"])&&isset($_POST["CheckList"]))
-{
-	if(sizeof($_POST["CheckList"])>0)
-	{
-		$website->ms_ia($_POST["CheckList"]);
-		$database->SQLDeletePlus("employer",$AuthUserName,"questionnaire","id",$_POST["CheckList"]);
-	}
-}
+<div class="row answerPollArea" style="margin-top: 20px;">
+    <div class="col-md-1">Câu trả lời: </div>
+    <div class="col-md-6"><input type="text" name="answerPoll[]" style="width: 50%;"> <button>Add more +</button></div>    
+</div>
 
-$website->ms_i($job_id);
-$arrJob=$database->DataArray("jobs","id=".$job_id);
-
-if($arrJob["employer"]!=$AuthUserName)
-{
-	die("");
-}
-$_REQUEST["message-column-width"]=150;
-
-
-$_REQUEST["arrNames2"]=array("employer","job_id");
-$_REQUEST["arrValues2"]=array($AuthUserName,$job_id);
-
-$_REQUEST["strSpecialHiddenFieldsToAdd"]=
-'
-	<input type="hidden" name="id" value="'.$job_id.'"/>
-';
-$insertID = AddNewForm
-(
-	array
-	(
-		$M_QUESTION.":",
-		$M_POSSIBLE_ANSWERS.":"
-	),
-	array
-	(
-		"question",
-		"answers"
-	),
-	array
-	(
-		"textbox_67",
-		"textarea_70_4"
-	),
-	$M_ADD,
-	"questionnaire",
-	$M_NEW_QUESTION_ADDED,
-	false,
-	array("","test")
-);
-?>
-
-<div class="clear"></div>
-<br/><br/>
-<?php
-
-if($database->SQLCount("questionnaire","WHERE job_id=".$job_id)==0)
-{
-?>
-	<br/>
-	<i><?php echo $M_STILL_NO_QUESTIONS;?></i>
-	<br/><br/><br/>
-<?php
-}
-else
-{
-	$_REQUEST["arrTDSizes"] = array("60","*","250");
-
-	RenderTable
-	(
-		"questionnaire",
-		array("EditPosting","question","show_answers"),
-		array($MODIFY,$M_QUESTION,$M_POSSIBLE_ANSWERS),
-		"100%",
-		"WHERE job_id=".$job_id,
-		$M_DELETE,
-		"id",
-		
-		"index.php?job_id=5"
-	);
-}
-?>
