0a034260aa76f43cf5ebd411ba5d8228220bc73d
unknown <dang.viet.son.hp@gmail.com>
2016-04-27 18:30:58 +0700

    jobseeker CV modification + search CV in employer area

--- JOBSEEKERS/home/apply_edit.php
+++ JOBSEEKERS/home/apply_edit.php
@@ -9,91 +9,73 @@ global $db;
 <div class="fright">
     <?php echo LinkTile("home","apply",$M_GO_BACK,"","red");?>
 </div>
-<div class="clear"></div>
-<h3>
-	<?php echo $M_JOB_DETAILS;?>
-</h3>
-<br/>
-
+    
+<h3><?php echo $M_JOB_DETAILS;?></h3>
+    
 <?php  
-
+    
 $id=$_REQUEST["id"];
 $website->ms_i($id);
-$application = $db->where('id', $id)->get('apply');
+$application = $db->where('id', $id)->getOne('apply');
 $Posting =  $db->join("categories", "jobsportal_jobs.job_category=jobsportal_categories.category_id", "LEFT")
             ->join("locations", "jobsportal_jobs.region=jobsportal_locations.id", "LEFT")
             ->join("salary", "jobsportal_jobs.salary=jobsportal_salary.salary_id", "LEFT")
-            ->where('jobsportal_jobs.id', $application[0]['posting_id'])->get('jobs');
-$employer = $db->where('username', $Posting[0]['employer'])->get('employers');
-
-//echo "<pre>";
-//print_r($Posting);
-//echo "</pre>";
-//$bad_string = "Hi! <script>alert('you are here');</script> It's a good day!";
-//$goodstring = $db->cleanData($bad_string);
-//print_r($goodstring);
-
+            ->where('jobsportal_jobs.id', $application['posting_id'])->getOne('jobs');
+                
+$employer = $db->where('username', $Posting['employer'])->getOne('employers');
+    
 ?>
+    
+<div class="row jobseeker-cv">
+    <div class="col-md-12 info-description row">
+        <section class="col-md-6">
+            <p>
+                <label><?php echo $JOB_TITLE?></label>
+                <span><?php echo stripslashes($Posting["title"])?></span>
+            </p>
 
-<div class="row">
-    <section class="col-md-12 info-section">
-        <label><?php echo $JOB_TITLE?></label>
-        <p><?php echo stripslashes($Posting[0]["title"])?></p>
-    </section>
-</div>
+            <p>
+                <label><?php echo $M_JOB_TYPE?>: </label>
+                <span><?php echo stripslashes($Posting["category_name_vi"])?></span>
+            </p>   
 
-<div class="row">
-    <section class="col-md-12 info-section">
+            <p>
+                <label><?php echo $M_REGION?>: </label>
+                <span><?php echo stripslashes($Posting["City"])?></span>
+            </p> 
+
+            <p>
+                <label><?php echo $M_SALARY?>: </label>
+                <span><?php echo stripslashes($Posting["salary_range"])?></span>
+            </p>
+        </section>    
+        <section class="col-md-6">
+            <p>
+                <label><?php echo $M_DATE_AVAILABLE?>: </label>
+                <span><?php echo date('Y-m-d',stripslashes($Posting["date"]))?></span>
+            </p>
+
+            <p>
+                <label><?php echo $M_COMPANY?>: </label>
+                <span><?php echo (trim($employer["company"])!=""?stripslashes($employer["company"]):"[n/a]")?></span>
+            </p>
+
+            <p>
+                <label><?php echo $COMPANY_DESCRIPTION?>: </label>
+                <span><?php echo stripslashes(trim($employer["company_description"])!=""?$employer["company_description"]:"[n/a]")?></span>
+            </p>
+
+            <p>
+                <label><?php echo $COMPANY_WEBSITE?>: </label>
+                <span><a href="<?php echo (trim($employer["website"])!=""?$employer["website"]:"[n/a]")?>"><?php echo (trim($employer["website"])!=""?$employer["website"]:"[n/a]")?></a></span>
+            </p>
+        </section>
+    </div>
+            
+    <section class="col-md-12 info-description">
         <label><?php echo $DESCRIPTION?></label>
-        <p><?php echo stripslashes($Posting[0]["message"])?></p>
+        <p><?php echo nl2br($Posting["message"])?></p>
     </section>
+            
+    
 </div>
-
-<div class="row">
-    <section class="col-md-12 info-section">
-        <label><?php echo $M_JOB_TYPE?>: </label>
-        <span><?php echo stripslashes($Posting[0]["category_name_vi"])?></span>
-    </section>
-</div>
-
-<div class="row">
-    <section class="col-md-12 info-section">
-        <label><?php echo $M_REGION?>: </label>
-        <span><?php echo stripslashes($Posting[0]["City"])?></span>
-    </section>
-</div>
-
-<div class="row">
-    <section class="col-md-12 info-section">
-        <label><?php echo $M_SALARY?>: </label>
-        <span><?php echo stripslashes($Posting[0]["salary_range"])?> USD</span>
-    </section>
-</div>
-
-<div class="row">
-    <section class="col-md-12 info-section">
-        <label><?php echo $M_DATE_AVAILABLE?>: </label>
-        <span><?php echo date('Y-m-d',stripslashes($Posting[0]["date"]))?></span>
-    </section>
-</div>
-
-<div class="row">
-    <section class="col-md-12 info-section">
-        <label><?php echo $M_COMPANY?>: </label>
-        <span><?php echo (trim($employer[0]["company"])!=""?stripslashes($employer[0]["company"]):"[n/a]")?></span>
-    </section>
-</div>
-
-<div class="row">
-    <section class="col-md-12 info-section">
-        <label><?php echo $COMPANY_DESCRIPTION?>: </label>
-        <p><?php echo stripslashes(trim($employer[0]["company_description"])!=""?$employer[0]["company_description"]:"[n/a]")?></p>
-    </section>
-</div>
-
-<div class="row">
-    <section class="col-md-12 info-section">
-        <label><?php echo $COMPANY_WEBSITE?>: </label>
-        <span><a href="<?php echo (trim($employer[0]["website"])!=""?$employer[0]["website"]:"[n/a]")?>"><?php echo (trim($employer[0]["website"])!=""?$employer[0]["website"]:"[n/a]")?></a></span>
-    </section>
-</div>		
