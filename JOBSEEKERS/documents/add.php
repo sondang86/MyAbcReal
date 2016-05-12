79c3315abd9f05032da0cd68cffc68a68039c9ba
Dang Viet Son <dang.viet.son.hp@gmail.com>
2016-05-13 00:21:21 +0700

    continue working on jobseekers upload form

--- JOBSEEKERS/documents/add.php
+++ JOBSEEKERS/documents/add.php
@@ -1,290 +1,64 @@
 <?php
 // Jobs Portal All Rights Reserved
-// A software product of NetArt Media, All Rights Reserved
-// Find out more about our products and services on:
-// http://www.netartmedia.net
-?><?php
+
 if(!defined('IN_SCRIPT')) die("");
+global $db, $commonQueries;
 ?>
-<link href="/vieclambanthoigian.com.vn/css/filepicker.css" rel="stylesheet" type="text/css"/>
-
-
-<body>
-    <?php require __DIR__.'/../../extensions/filepicker/_navbar.php'; ?>
-    
-   <?php // print_r(__DIR__.'/../files');?>
-
-    <div class="container">
-        <div class="demo-container col-md-9 col-md-offset-2">
-            <?php demo_nav(); ?>
-
-            <div id="filepicker">
-                <!-- Button Bar -->
-                <div class="button-bar">
-                    <div class="btn btn-success fileinput">
-                        <i class="fa fa-plus"></i> Add files
-                        <input type="file" name="files[]" multiple>
-                    </div>
-
-                    <button type="button" class="btn btn-primary camera-show">
-                        <i class="fa fa-camera"></i> Camera
-                    </button>
-
-                    <button type="button" class="btn btn-info start-all">
-                        <i class="fa fa-arrow-circle-o-up"></i> Start all
-                    </button>
-
-                    <button type="button" class="btn btn-warning cancel-all">
-                        <i class="fa fa-ban"></i> Cancel all
-                    </button>
-
-                    <button type="button" class="btn btn-danger delete-all">
-                        <i class="fa fa-trash-o"></i> Delete all
-                    </button>
-                </div>
-
-                <!-- Files -->
-                <div class="table-responsive">
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th class="column-preview">Preview</th>
-                                <th class="column-name">Name</th>
-                                <th class="column-size">Size</th>
-                                <th class="column-date">Modified</th>
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody class="files"></tbody>
-                        <tfoot><tr><td colspan="5">No files where found.</td></tr></tfoot>
-                    </table>
-                </div>
-
-                <!-- Pagination -->
-                <div class="pagination-container text-center"></div>
-
-                <!-- Drop Window -->
-                <div class="drop-window">
-                    <div class="drop-window-content">
-                        <h3><i class="fa fa-upload"></i> Drop files to upload</h3>
-                    </div>
-                </div>
-            </div><!-- end of #filepicker -->
-        </div>
-    </div>
-
-    <!-- Crop Modal -->
-    <div id="crop-modal" class="modal fade" tabindex="-1">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <span class="close" data-dismiss="modal">&times;</span>
-                    <h4 class="modal-title">Make a selection</h4>
-                </div>
-                <div class="modal-body">
-                    <div class="alert alert-warning crop-loading">Loading image...</div>
-                    <div class="crop-preview"></div>
-                </div>
-                <div class="modal-footer">
-                    <div class="crop-rotate">
-                        <button type="button" class="btn btn-default btn-sm crop-rotate-left" title="Rotate left">
-                            <i class="fa fa-undo"></i>
-                        </button>
-                        <button type="button" class="btn btn-default btn-sm crop-flip-horizontal" title="Flip horizontal">
-                            <i class="fa fa-arrows-h"></i>
-                        </button>
-                        <!-- <button type="button" class="btn btn-default btn-sm crop-flip-vertical" title="Flip vertical">
-                            <i class="fa fa-arrows-v"></i>
-                        </button> -->
-                        <button type="button" class="btn btn-default btn-sm crop-rotate-right" title="Rotate right">
-                            <i class="fa fa-repeat"></i>
-                        </button>
-                    </div>
-                    <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Cancel</button>
-                    <button type="button" class="btn btn-success crop-save" data-loading-text="Saving...">Save</button>
-                </div>
-            </div>
-        </div>
-    </div><!-- end of #crop-modal -->
-
-    <!-- Camera Modal -->
-    <div id="camera-modal" class="modal fade" tabindex="-1">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <span class="close" data-dismiss="modal">&times;</span>
-                    <h4 class="modal-title">Take a picture</h4>
-                </div>
-                <div class="modal-body">
-                    <div class="camera-preview"></div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default pull-left camera-hide" data-dismiss="modal">Cancel</button>
-                    <button type="button" class="btn btn-success camera-capture">Take picture</button>
-                </div>
-            </div>
-        </div>
-    </div><!-- end of #camera-modal -->
-
-    <!-- Scripts -->
-    <script src="https://code.jquery.com/jquery-1.12.3.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-timeago/1.5.2/jquery.timeago.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/cropper/2.3.0/cropper.min.js"></script>
-
-    <script src="/vieclambanthoigian.com.vn/extensions/filepicker/assets/js/filepicker.js"></script>
-    <script src="/vieclambanthoigian.com.vn/extensions/filepicker/assets/js/filepicker-ui.js"></script>
-    <script src="/vieclambanthoigian.com.vn/extensions/filepicker/assets/js/filepicker-drop.js"></script>
-    <script src="/vieclambanthoigian.com.vn/extensions/filepicker/assets/js/filepicker-crop.js"></script>
-    <script src="/vieclambanthoigian.com.vn/extensions/filepicker/assets/js/filepicker-camera.js"></script>
-
-    <script>
-        /*global $*/
-        $('#filepicker').filePicker({
-            url: '/vieclambanthoigian.com.vn/extensions/filepicker/uploader/index.php',
-            ui: {
-                autoUpload: false
-            },
-            plugins: ['ui', 'drop', 'camera', 'crop'],
-            
-            
-        });
+<?php
+    if(isset($_POST['submit'])){
+//        print_r($_FILES);
+        $storage = new \Upload\Storage\FileSystem('../user_files/jobseekers');
+        $file = new \Upload\File('file', $storage);
+        // Optionally you can rename the file on upload
+        $new_filename = $file->setName('1');
         
+        echo "<pre>";
+        print_r($file->getName());
+        echo "</pre>";
+        
+        // Validate file upload
+        // MimeType List => http://www.iana.org/assignments/media-types/media-types.xhtml
+        $file->addValidations(array(
+            // Ensure file is of type "image/png"
+            new \Upload\Validation\Mimetype(array('application/pdf', 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document')),
 
-        // Replace timeago strings.
-        if ($.fn.timeago) {
-            $.timeago.settings.strings = $.extend({}, $.timeago.settings.strings , {
-                seconds: 'few seconds', minute: 'a minute',
-                hour: 'an hour', hours: '%d hours', day: 'a day',
-                days: '%d days', month: 'a month', year: 'a year'
-            });
+            //You can also add multi mimetype validation
+            //new \Upload\Validation\Mimetype(array('image/png', 'image/gif'))
+
+            // Ensure file is no larger than 5M (use "B", "K", M", or "G")
+            new \Upload\Validation\Size('5M')
+        ));
+
+        // Access data about the file that has been uploaded
+        $data = array(
+            'name'       => $file->getNameWithExtension(),
+            'extension'  => $file->getExtension(),
+            'mime'       => $file->getMimetype(),
+            'size'       => $file->getSize(),
+            'md5'        => $file->getMd5(),
+            'dimensions' => $file->getDimensions()
+        );
+
+        //delete old files if same userId, allowed store only one file 
+        if ($file->getName() == "1"){
+//            unlink("../user_files/jobseekers/1.*");
+            $mask = '../user_files/jobseekers/1.*';
+            array_map('unlink', glob($mask));
         }
-    </script>
-
-    <!-- Upload Template -->
-    <script type="text/x-tmpl" id="uploadTemplate">
-        <tr class="upload-template">
-            <td class="column-preview">
-                <div class="preview">
-                    <span class="fa file-icon-{%= o.file.extension %}"></span>
-                </div>
-            </td>
-            <td class="column-name">
-                <p class="name">{%= o.file.name %}</p>
-                <span class="text-danger error">{%= o.file.error || '' %}</span>
-            </td>
-            <td colspan="2">
-                <p>{%= o.file.sizeFormatted || '' %}</p>
-                <div class="progress">
-                    <div class="progress-bar progress-bar-striped active"></div>
-                </div>
-            </td>
-            <td>
-                {% if (!o.file.autoUpload && !o.file.error) { %}
-                    <a href="#" class="action action-primary start" title="Upload">
-                        <i class="fa fa-arrow-circle-o-up"></i>
-                    </a>
-                {% } %}
-                <a href="#" class="action action-warning cancel" title="Cancel">
-                    <i class="fa fa-ban"></i>
-                </a>
-            </td>
-        </tr>
-    </script><!-- end of #uploadTemplate -->
-
-    <!-- Download Template -->
-    <script type="text/x-tmpl" id="downloadTemplate">
-        {% o.timestamp = function (src) {
-            return (src += (src.indexOf('?') > -1 ? '&' : '?') + new Date().getTime());
-        }; %}
-        <tr class="download-template">
-            <td class="column-preview">
-                <div class="preview">
-                    {% if (o.file.versions && o.file.versions.thumb) { %}
-                        <a href="/vieclambanthoigian.com.vn/extensions/filepicker/{%= o.file.url %}" target="_blank">
-                            <img src="/vieclambanthoigian.com.vn/extensions/filepicker/{%= o.timestamp(o.file.versions.thumb.url) %}" width="64" height="64"></a>
-                        </a>
-                    {% } else { %}
-                        <span class="fa file-icon-{%= o.file.extension %}"></span>
-                    {% } %}
-                </div>
-            </td>
-            <td class="column-name">
-                <p class="name">
-                    {% if (o.file.url) { %}
-                        <a href="/vieclambanthoigian.com.vn/extensions/filepicker/{%= o.file.url %}" target="_blank">{%= o.file.name %}</a>
-                    {% } else { %}
-                        {%= o.file.name %}
-                    {% } %}
-                </p>
-                {% if (o.file.error) { %}
-                    <span class="text-danger">{%= o.file.error %}</span>
-                {% } %}
-            </td>
-            <td class="column-size"><p>{%= o.file.sizeFormatted %}</p></td>
-            <td class="column-date">
-                {% if (o.file.time) { %}
-                    <time datetime="{%= o.file.timeISOString() %}">
-                        {%= o.file.timeFormatted %}
-                    </time>
-                {% } %}
-            </td>
-            <td>
-                {% if (o.file.imageFile && !o.file.error) { %}
-                    <a href="#" class="action action-primary crop" title="Crop">
-                        <i class="fa fa-crop"></i>
-                    </a>
-                {% } %}
-                {% if (o.file.error) { %}
-                    <a href="#" class="action action-warning cancel" title="Cancel">
-                        <i class="fa fa-ban"></i>
-                    </a>
-                {% } else { %}
-                    <a href="#" class="action action-danger delete" title="Delete">
-                        <i class="fa fa-trash-o"></i>
-                    </a>
-                {% } %}
-            </td>
-        </tr>
-    </script><!-- end of #downloadTemplate -->
-
-    <!-- Pagination Template -->
-    <script type="text/x-tmpl" id="paginationTemplate">
-        {% if (o.lastPage > 1) { %}
-            <ul class="pagination pagination-sm">
-                <li {% if (o.currentPage === 1) { %} class="disabled" {% } %}>
-                    <a href="#!page={%= o.prevPage %}" data-page="{%= o.prevPage %}" title="Previous">&laquo;</a>
-                </li>
-
-                {% if (o.firstAdjacentPage > 1) { %}
-                    <li><a href="#!page=1" data-page="1">1</a></li>
-                    {% if (o.firstAdjacentPage > 2) { %}
-                       <li class="disabled"><a>...</a></li>
-                    {% } %}
-                {% } %}
-
-                {% for (var i = o.firstAdjacentPage; i <= o.lastAdjacentPage; i++) { %}
-                    <li {% if (o.currentPage === i) { %} class="active" {% } %}>
-                        <a href="#!page={%= i %}" data-page="{%= i %}">{%= i %}</a>
-                    </li>
-                {% } %}
-
-                {% if (o.lastAdjacentPage < o.lastPage) { %}
-                    {% if (o.lastAdjacentPage < o.lastPage - 1) { %}
-                        <li class="disabled"><a>...</a></li>
-                    {% } %}
-                    <li><a href="#!page={%= o.lastPage %}" data-page="{%= o.lastPage %}">{%= o.lastPage %}</a></li>
-                {% } %}
-
-                <li {% if (o.currentPage === o.lastPage) { %} class="disabled" {% } %}>
-                    <a href="#!page={%= o.nextPage %}" data-page="{%= o.nextPage %}" title="Next">&raquo</a>
-                </li>
-            </ul>
-        {% } %}
-    </script><!-- end of #paginationTemplate -->
-
-</body>
-
+        
+        //Upload file info to db
+        
+        // Try to upload file
+        try {
+            // Success!
+            $file->upload();
+            
+        } catch (\Exception $e) {
+            // Fail!
+            $errors = $file->getErrors();
+        }
+    }
+?>
 
 <div class="row">
     <section class="col-md-6"></section>
@@ -296,35 +70,35 @@ if(!defined('IN_SCRIPT')) die("");
     </section>
 </div>
 
-<form action="demo-order-process.php" method="post" enctype="multipart/form-data" id="sky-form" class="sky-form">
-    <header>Order services</header>
-        
+<form action="" method="post" enctype="multipart/form-data" id="sky-form" class="sky-form">
+    <header>Tùy chỉnh tập tin cá nhân của bạn</header>
+    
     <fieldset>
         <div class="row">
             <section class="col col-6">
                 <label class="input">
                     <i class="icon-append fa fa-user"></i>
-                    <input type="text" name="name" placeholder="Name" required>
+                    <input type="text" name="name" placeholder="tiêu đề tập tin" required>
                 </label>
             </section>
         </div>      
         
         <section>
-                <label for="file" class="input input-file">
-                        <div class="button"><input type="file" name="file" multiple onchange="this.parentNode.nextSibling.value = this.value">Browse</div><input type="text" placeholder="Include some file" readonly>
-                </label>
+            <label for="file" class="input input-file">
+                <div class="button"><input type="file" name="file" onchange="this.parentNode.nextSibling.value = this.value">Browse</div><input type="text" placeholder="Include some file" readonly>
+            </label>
         </section>
         
         <section>
             <label class="textarea">
                 <i class="icon-append fa fa-comment"></i>
-                <textarea rows="5" name="comment" placeholder="Tell us about your project"></textarea>
+                <textarea rows="5" name="comment" placeholder="Mô tả tập tin"></textarea>
             </label>
         </section>
-        					
+        
     </fieldset>
     <footer>
-        <button type="submit" class="button">Send request</button>
+        <button type="submit" name="submit" class="button">Send request</button>
         <div class="progress"></div>
     </footer>				
     <div class="message">
@@ -335,42 +109,44 @@ if(!defined('IN_SCRIPT')) die("");
 </div>
 
 <script type="text/javascript">
-$(function()
-{
-    // Validation
-    $("#sky-form").validate(
-    {					
-        // Rules for form validation
-        rules:
-        {
-            name:{
-                required: true
+    $(function()
+    {
+        // Validation
+        $("#sky-form").validate(
+                {					
+                    // Rules for form validation
+            rules:
+                    {
+                        name:{
+                            required: true
+                },
+                file:{
+                    required: 'Bạn chưa upload file',
+//                    extension: "xls|csv",
+                    accept: "application/pdf, application/vnd.openxmlformats-officedocument.wordprocessingml.document, application/msword"
+                }
             },
-            file:{
-                required: 'Bạn chưa upload file',
-                extension: "xls|csv"
-            }
-        },
 
-        // Messages for form validation
-        messages:
-        {
-            name:
+            // Messages for form validation
+            messages:
+                    {
+                        name:
+                        {
+                            required: 'Please enter your name'
+                },
+                file:{
+                    required: 'Bạn chưa upload file',
+//                    extension: "sdadasd",
+                    accept: 'Chỉ chấp nhận pdf và doc/docx file'
+                }
+            },
+
+            // Do not change code below
+            errorPlacement: function(error, element)
             {
-                required: 'Please enter your name'
-            },
-            file:{
-                required: 'Bạn chưa upload file',
-                extension: "sdadasd"
+                error.insertAfter(element.parent());
             }
-        },
-
-        // Do not change code below
-        errorPlacement: function(error, element)
-        {
-            error.insertAfter(element.parent());
-        }
+        });
     });
-});
 </script>
 
